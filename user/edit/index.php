feee75f97cb7b8bedeead3c99e7b19ed42f023b2
nash <nash.0cal@gmail.com>
2020-01-20 19:16:08 +0900

    画像名をDBに保存

--- user/edit/index.php
+++ user/edit/index.php
@@ -23,6 +23,17 @@ if(isset($_POST['edit']) || isset($_PUT['edit'])){
     }else{
         echo "名前・パスワード両方入力ください";
     }  
+}elseif(isset($_POST['send'])  || isset($_PUT['send'])){
+    $id   = $_SESSION['id'];
+    try{
+        $pdo    = new PDO(DB_DNS,DB_NAME,DB_PASS);
+        $filename = $_POST['file'];
+        $query  = "UPDATE users SET image = '$filename'  WHERE id='$id' ";
+        $resurt = $pdo->query($query)->fetch(PDO::FETCH_ASSOC);
+        header('Location: http://localhost/php');
+    }catch(PDOException $e){
+        echo $e->getMessage;
+    }
 }
 
 ?>
@@ -33,10 +44,17 @@ if(isset($_POST['edit']) || isset($_PUT['edit'])){
 <meta charset='UTF-8'>
 </head>
 <body>
+
+
+<form method="post" action="http://localhost/php/user/edit/">
+    <input type="file" name="file"><br><br>
+<button type='submit' class="btn btn-default" name="send">画像を変える</button>
+</form>
+
 <form method="post" action="http://localhost/php/user/edit/">
     <h1>フォーム修正</h1>
-    <input type="name" name="name">
-    <input type="password" name="password">
+    <input type="name" name="name"><br><br>
+    <input type="password" name="password"><br><br>
 <button type='submit' class="btn btn-default" name="edit">編集する</button>
 </form>
 </body>
